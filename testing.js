89dcd7e37cdcd9a1304707e459f944d2cf4dd0e7
Allen Lee <allenlee@mit.edu>
2018-05-03 23:47:09 -0400

    movement tracking

--- testing.js
+++ testing.js
@@ -7,13 +7,14 @@ var ctx = canvas.getContext("2d");
 // Setup Leap loop with frame callback function
 var controllerOptions = {enableGestures: true};
 
-var colorMap = ["red", "blue", "green", "black", "purple"]
-var lastThreePositions = [0, 1, 2]
-var tempPositions = [0, 1, 2, 3, 4]
-var counter = 0
+var colorMap = ["red", "blue", "green", "black", "purple"];
+var lastThreePositions = [0, 1, 2];
+var tempPositions = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]];
+var counter = 0;
+var change = false;
+
 Leap.loop({frameEventName: "animationFrame"}, function(frame) {
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
 
         frame.pointables.forEach(function(pointable) {
 
@@ -28,10 +29,26 @@ Leap.loop({frameEventName: "animationFrame"}, function(frame) {
           ctx.fillRect(x, y, 20, 20);
         });
         
+        
 });
 
 setInterval(function() {
-          lastThreePositions[counter % 3] = tempPositions;
+          lastThreePositions[counter % 3] = tempPositions.slice();
           counter += 1;
-          console.log(lastThreePositions)
-}, 1000);
+          // console.log(lastThreePositions[0])
+          // console.log(lastThreePositions)
+          checkMovement(lastThreePositions);
+}, 200);
+
+function checkMovement(lastThreePositions) {
+    var fingers_one = lastThreePositions[0];
+    var fingers_two = lastThreePositions[1];
+    var fingers_three = lastThreePositions[2];
+    // console.log("This is fingers one", fingers_one)
+    // console.log(fingers_one[2][0], " | ", fingers_two[2][0], " | ", fingers_three[2][0])
+    for (var i = 0; i < fingers_one.length; i++){
+       if(fingers_one[2][0] < fingers_two[2][0] && fingers_two[2][0] < fingers_three[2][0]) {
+          console.log("moved left to right");
+       }
+    }
+}
